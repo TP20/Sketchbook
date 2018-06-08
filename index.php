9589891810f5ec1d4815456440970bb2ef4d485d
TP20 <thujan2@gmail.com>
2018-06-08 17:08:42 -0400

    modified registration query

--- index.php
+++ index.php
@@ -70,9 +70,8 @@
         <br> All Right Reserved  <span class="glyphicon glyphicon-copyright-mark" aria-hidden="true"></span> </p>
     </div> -->
 
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 </body>
 
